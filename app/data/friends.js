05215f84ba20209bf566e6dcbe51b948663d570a
djknit <djknit@gmail.com>
2018-11-13 15:15:46 -0600

    dummies, clear inputs

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,6 +12,86 @@ let friends = [
             4,
             4,
             2,
+            2
+        ]
+    },
+    {
+        "name": "Harry Potter",
+        "picture": "https://images.pottermore.com/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=1200",
+        "answers": [
+            2,
+            4,
+            3,
+            4,
+            5,
+            3,
+            3,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Eric Cartman",
+        "picture": "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/EricCartman.png/220px-EricCartman.png",
+        "answers": [
+            5,
+            1,
+            1,
+            5,
+            3,
+            1,
+            1,
+            2,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Professor Hubert J. Farnsworth",
+        "picture": "https://www.syfy.com/sites/syfy/files/cast_futurama_proffarnsworth.jpg",
+        "answers": [
+            4,
+            2,
+            2,
+            5,
+            5,
+            1,
+            3,
+            4,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Bender",
+        "picture": "https://imgix.ranker.com/user_node_img/50060/1001188616/original/bender-turns-into-a-criminal-in-the-first-episode-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces",
+        "answers": [
+            5,
+            2,
+            1,
+            5,
+            5,
+            1,
+            1,
+            3,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Philip J. Fry",
+        "picture": "https://upload.wikimedia.org/wikipedia/en/2/28/Philip_Fry.png",
+        "answers": [
+            3,
+            2,
+            1,
+            5,
+            3,
+            4,
+            1,
+            3,
+            4,
             1
         ]
     }

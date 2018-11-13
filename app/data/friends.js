13f60907e4b211ffd16c68737238a4f767a3d24b
djknit <djknit@gmail.com>
2018-11-13 14:46:07 -0600

    changed question

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,20 @@
-let friends = [];
+let friends = [
+    {
+        "name": "David Knittel",
+        "picture": "https://i.imgur.com/i0eSM81.jpg",
+        "answers": [
+            2,
+            4,
+            2,
+            4,
+            5,
+            5,
+            4,
+            4,
+            2,
+            1
+        ]
+    }
+];
 
 module.exports = friends;
